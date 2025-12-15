63c86adf5081e5362cffda26db4bd795d62ded30
Pedro Henrique Padilha Portella da Cruz <pedro.kadjin.sg@gmail.com>
2025-12-15 19:47:22 -0300

    feat: adjust tests

--- complex/client/src/App.test.js
+++ complex/client/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should render de App component', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
